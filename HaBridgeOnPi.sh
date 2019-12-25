808f7cc8c4de5f1950c3ce0280d0d9f8b9217e29
Piyush Kumar <piyushkumar.jiit@gmail.com>
2019-12-25 00:58:06 -0600

    Add files via upload

--- HaBridgeOnPi.sh
+++ HaBridgeOnPi.sh
@@ -1,61 +1,77 @@
 #!bin/bash
+set -e
 userName=$(whoami)
+openJDKLink='https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.deb'
+oracleJDKLink=''
+haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0-java11.jar'
 
 #In case you want OpenJDK 11
-wget https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.deb
+wget -O JDK.deb $openJDKLink
+echo "Downloaded OpenJDK."
 
-#In case you want to use Oracle JDK, Download the JDK 11 from 
+#Install OpenJDK
+sudo apt-get install ./JDK.deb
+
+sudo update-alternatives --config javac
+sudo update-alternatives --config java
+
+#In case you want to use Oracle JDK, Download the JDK 11 from
+#wget oracleJDKLink
 #Unzip the JDK 
 
-#Create the HABridge Directory
-sudo mkdir /etc/habridge
-sudo chown -R userName:userName /etc/habridge
+#Create the HA Bridge Directory
+if [ -d "/etc/habridge" ] 
+then
+    echo "/etc/habridge Directory exists." 
+else
+    echo "Directory /etc/habridge does not exists. Creating directory"
+	sudo mkdir /etc/habridge
+fi
+
 cd /etc/habridge
 
 #Fetch the HA Bridge jar
-sudo wget https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0-java11.jar
+sudo wget -O ha-bridge.jar haBridgeLink
+
+#Add habridgeadmin User
+user_exists=$(id -u habridgeadmin > /dev/null 2>&1; echo $?)
+if [[ -z $user_exists ]]
+then
+	echo "Adding user"
+	sudo useradd -s /usr/sbin/nologin -r -M habridgeadmin
+else
+	echo "User exists. Continuing without adding."
+fi
 
 #Update things
 sudo apt-get update
 
-
 #Run HA Bridge via command prompt
 #sudo java -jar -Dserver.port=8080 ha-bridge-3.5.1.jar
 
-#Provide Service Name
-echo "Enter Service Name:"
-read serviceName
+#Download the service file from github
+wget https://raw.githubusercontent.com/piyushkumarjiit/HABridgeOnPi/master/HABridge.service
+echo "Service file donwloaded."
 
-echo "Enter Service Description:"
-read serviceDescription
+#Update the service file
+sed -i "s/userName/${userName}/g" HABridge.service
+sed -i "s/userName/${userName}/g" HABridge.service
+echo "Updated service file"
 
-#Create service in system with provided name
-sudo cat <<EOF >/etc/systemd/system/serviceName.service
+#Copy the service file to system directory
+sudo mv HABridge.service /etc/systemd/system/
 
-Description=serviceDescription
-
-Wants=network.target
-After=syslog.target network-online.target
-
-[Service]
-Type=simple
-ExecStart=YOUR_COMMAND_HERE
-Restart=on-failure
-RestartSec=10
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-EOF
+#update Permission on the HA Bridge direcotry
+sudo chown -R habridgeadmin:habridgeadmin /etc/habridge
 
 #Restart daemon
 sudo systemctl daemon-reload
 
 #Enable the newly created service
-sudo systemctl enable serviceName
+sudo systemctl enable HABridge
 
 #Start the service
-sudo systemctl start serviceName
+sudo systemctl start HABridge
 
 #Check the status of service
-systemctl status serviceName
+systemctl status HABridge
