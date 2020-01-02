51b57944409d86dbb1d02d76b9c9dd073f41788e
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-01-02 01:47:34 -0600

    Add files via upload

--- HaBridgeOnPi.sh
+++ HaBridgeOnPi.sh
@@ -4,23 +4,22 @@ set -e
 #userName=$(whoami)
 #openJDK11Link='https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz'
 #openJDK13ForZeroLink="https://cdn.azul.com/zulu/bin/zulu13.28.11-ca-jdk13.0.1-linux_amd64.deb"
-#oracleJDKLink=''
 #haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0-java11.jar'
 
 #Check if Java is already installed
 java_present=$(java -version > /dev/null 2>&1; echo $?)
 
 #Check the Model of Pi. There needs to be a different binary and Java for Zero (ARMv6)
-	model=$(cat /proc/cpuinfo | grep Model | grep -e "Zero" -e "Model A")
-	if [[ -n $model ]]
-	then
-		echo "Pi Zero detected. Downloading old HA binary."
-		#HA_Version5.2.2 works with OpenJDK_8 while Later versions of HA do not
-		haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.2.2/ha-bridge-5.2.2.jar'	
-	else
-		echo "Not Pi Zero Model. Downloading recent/latest HA binary."
-		haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0-java11.jar'
-	fi
+model=$(cat /proc/cpuinfo | grep Model | grep -e "Zero" -e "Model A")
+if [[ -n $model ]]
+then
+	echo "Pi Zero detected. Downloading old HA binary."
+	#HA_Version5.2.2 works with OpenJDK_8 while Later versions of HA do not
+	haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.2.2/ha-bridge-5.2.2.jar'	
+else
+	echo "Not Pi Zero Model. Downloading recent/latest HA binary."
+	haBridgeLink='https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0-java11.jar'
+fi
 
 #Fetch the HA Bridge JAR file
 sudo wget -O ha-bridge.jar $haBridgeLink
@@ -108,8 +107,44 @@ else
 	sudo systemctl start HABridge
 fi
 
-
 #Check the status of service
 systemctl status HABridge
+
+#Create the RFOutlet Directory
+if [[ -d "/var/www/rfoutlet" ]]
+then
+	echo "/var/www/rfoutlet Directory exists. Skipping download."
+else
+	#Install wiringpi if not already installed and fetch the project from github
+	wiringpi_present=$(gpio -v > /dev/null 2>&1; echo $?)
+	if [[ $wiringpi_present -le "1" ]]
+	then
+		echo "Installing WiringPi."
+		sudo apt-get install wiringpi
+		echo "WiringPi installed."
+	else
+		echo "WiringPi is present. Continuing without adding."
+	fi
+
+	echo "Downloading RFOutlet source."
+	git clone git://github.com/timleland/rfoutlet.git /var/www/rfoutlet
+	
+	#sudo mkdir -p /var/www/rfoutlet
+	#Just donwload the binary. In case of issue we will have to download the source and build from it. For now it seems to work on Pi 3 as well as Pi Zero.
+	#sudo wget -O /var/www/rfoutlet/codesend https://github.com/timleland/rfoutlet/raw/master/codesend
+	#RFSNiffer file link to be added
+	
+	echo "Ensure PINs are connected in order GPIO17 | 5V | Ground when the transmitter's non flat side is facing you."
+	#Update the permissions
+	sudo chown root.root /var/www/rfoutlet/codesend
+	sudo chmod 755 /var/www/rfoutlet/codesend
+	echo "Permission updated."
+fi
+
+#To Sniff the RF Code
+#sudo /var/www/rfoutlet/RFSniffer
+
+#To send the RF Code
+#/var/www/rfoutlet/codesend <RFDecimalCode>
 
 echo "Script complete."
